d35dc01b4d9c33596d901014a83d0718225f4b21
Tom Bell <ctombell@gmail.com>
2016-05-14 12:37:35 +1200

    Got the compute buttons to write some text to the page

--- script/euler.js
+++ script/euler.js
@@ -1,11 +1,24 @@
+/*function init() {
+    var form1 = document.getElementById('form_problem1');
+    form1.addEventListener('submit',problem1,false);
+}
+function problem1(e) {
+    alert('it worked!');
+    document.getElementById('answer1').innerHTML = 'the answer';
+    e.preventDefault();
+    return false;
+}*/
 function problem1() {
+    document.getElementById('answer1').innerHTML = 'the answer';
     return false;
 }
 
 function problem2() {
+    document.getElementById('answer2').innerHTML = 'the answer';
     return false;
 }
 
 function problem3() {
+    document.getElementById('answer3').innerHTML = 'the answer';
     return false;
 }
