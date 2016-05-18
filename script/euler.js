d5f9ad2b7a14b1a6b85cb14ddac87b085e1d24bd
Tom Bell <ctombell@gmail.com>
2016-05-18 18:20:05 +1200

    problem one done

--- script/euler.js
+++ script/euler.js
@@ -10,7 +10,15 @@ function problem1(e) {
 }*/
 function problem1() {
     var input1 = document.getElementById('problem1_input').value;
-    document.getElementById('answer1').innerHTML = input1;
+    var sum = 0;
+    for(var i = 0; i < input1; i++){
+        if(i % 3 === 0){
+            sum += i;
+        } else if(i % 5 === 0){
+            sum += i;
+        }
+    }
+    document.getElementById('answer1').innerHTML = sum;
     return false;
 }
 
