83370b67fb2359f1550ae8924045afd69198c3c7
Tom Bell <ctombell@gmail.com>
2016-05-19 16:49:09 +1200

    Complete problem two

--- script/euler.js
+++ script/euler.js
@@ -1,13 +1,3 @@
-/*function init() {
-    var form1 = document.getElementById('form_problem1');
-    form1.addEventListener('submit',problem1,false);
-}
-function problem1(e) {
-    alert('it worked!');
-    document.getElementById('answer1').innerHTML = 'the answer';
-    e.preventDefault();
-    return false;
-}*/
 function problem1() {
     var input1 = document.getElementById('problem1_input').value;
     var sum = 0;
@@ -26,18 +16,17 @@ function problem2() {
     var input2 = document.getElementById('problem2_input').value;
     var fibonacci = [1,2];
     var sum = 0;
-    for(i = 2; i < input2; i++){
+    for(i = 1; i < input2; i++){
         if(fibonacci[i] > input2){
-            break; //this doesn't currently do what I want it to
+            break;
         }
-        fibonacci.push(fibonacci[i-1] + fibonacci[i-2]);
+        fibonacci.push(fibonacci[i] + fibonacci[i-1]);
     }
     for(i = 0; i < fibonacci.length; i++){
         if(fibonacci[i] % 2 === 0){
             sum+=fibonacci[i];
         }
     }
-    alert(fibonacci);
     document.getElementById('answer2').innerHTML = sum;
     return false;
 }
